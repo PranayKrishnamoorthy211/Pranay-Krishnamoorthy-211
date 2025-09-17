d440927ece1e2285f6289063806697a0bb39e184
PranayKrishnamoorthy211 <pranaykrishnamoorthy@gmail.com>
2025-09-17 04:21:31 +0000

    added code but there is a small error

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -1,75 +1,122 @@
 package robot;
 
+
 import kareltherobot.*;
+
 
 public class Roomba implements Directions {
 
-    // Main method to make this self-contained
-    public static void main(String[] args) {
-        // LEAVE THIS ALONE!!!!!!
-        String worldName = "robot/basicRoom.wld";
 
-        Roomba cleaner = new Roomba();
-        int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
-        //System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
-
-    }
-
-    // declared here so it is visible in all the methods!
-    private Robot roomba;
-
-    // You will need to add many variables!!
+   public static void main(String[] args) {
+       String worldName = "robot/finalTestWorld2024.wld";
 
 
-    public int cleanRoom(String worldName, int startX, int startY) {
+       Roomba cleaner = new Roomba();
+       int totalBeepers = cleaner.cleanRoom(worldName, 26, 101);
 
-        // A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
-        // Make sure it starts at startX and startY location.
 
-        World.readWorld(worldName);
-        World.setVisible(true);
-        //World.setSize(20, 20);
-        World.setDelay(1);
+   }
 
-        roomba = new Robot(startX, startY, East, 0);
-        int count = 0;
-        int numPiles = 0; 
-        boolean end = true; 
-        while(end){
-            while(roomba.frontIsClear()){
-                roomba.move(); 
-                if(roomba.nextToABeeper() == true){
-                    numPiles++; 
-                }
-                while(roomba.nextToABeeper()){
-                    roomba.pickBeeper();
-                    count++; 
-                }
-                
 
-            }
-            if(roomba.facingEast() && !roomba.frontIsClear()){
-                roomba.turnLeft();
-                roomba.move(); 
-                roomba.turnLeft(); 
-            }
-            if(roomba.facingWest() && !roomba.frontIsClear()){
-                turnRight(roomba);
-                roomba.move(); 
-                turnRight(roomba); 
-            }
-            if(roomba.facingNorth() && !roomba.frontIsClear()){
-                end = false; 
-            }
-        } 
-        System.out.println("Total number of beepers: "+ count); 
-        System.out.println("Total number of piles: "+numPiles); 
+   private Robot roomba;
 
-        return count;}
+
+
+
+
+
+   public int cleanRoom(String worldName, int startX, int startY) {
+
+
+  
+
+
+       World.readWorld(worldName);
+       World.setVisible(true);
+       World.setDelay(0);
+
+
+       roomba = new Robot(startX, startY, East, 0);
+       int count = 0;
+       int numPiles = 0;
+       int numSteps = 0;
+       boolean end = true;
+       while(end){
+           while(roomba.nextToABeeper()){
+                   roomba.pickBeeper();
+                   count++;
+
+
+               }
+           while(roomba.frontIsClear()){
+               roomba.move();
+               numSteps++;
+               if(roomba.nextToABeeper() == true){
+                   numPiles++;
+               }
+               while(roomba.nextToABeeper()){
+                   roomba.pickBeeper();
+                   count++;
+
+
+               }
+  
+           }
+           if(roomba.facingEast() && !roomba.frontIsClear()){
+               while(roomba.nextToABeeper()){
+                   roomba.pickBeeper();
+                   count++;
+
+
+               }
+               roomba.turnLeft();
+               roomba.move();
+               numSteps++;
+               while(roomba.nextToABeeper()){
+                   roomba.pickBeeper();
+                   count++;
+
+
+               }
+               roomba.turnLeft();
+           }
+           if(roomba.facingWest() && !roomba.frontIsClear()){
+               while(roomba.nextToABeeper()){
+                   roomba.pickBeeper();
+                   count++;
+
+
+               }
+               turnRight(roomba);
+               roomba.move();
+               numSteps++;
+               while(roomba.nextToABeeper()){
+                   roomba.pickBeeper();
+                   count++;
+
+
+               }
+               turnRight(roomba);
+           }
+           if(roomba.facingNorth() && !roomba.frontIsClear()){
+               end = false;
+           }
+       }
+       System.out.println("Total number of beepers: "+ count);
+       System.out.println("Dirty percentage: "+ (double)numPiles/numSteps +"%");
+       System.out.println("Average pile size: "+ (double)count/numPiles);
+       System.out.println("Area: "+numSteps);
+       System.out.println("Total number of piles: "+ numPiles);
+       return count;
       
-	public static void turnRight(Robot roomba){
-	roomba.turnLeft();
-	roomba.turnLeft();
-	roomba.turnLeft();
-	}
+  
+   }
+
+
+   public static void turnRight(Robot roomba){
+       roomba.turnLeft();
+       roomba.turnLeft();
+       roomba.turnLeft();
+   }
 }
+ 
