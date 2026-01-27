22823b8d97be20eba3673123a5f39acee908b3b9
PranayKrishnamoorthy211 <pranaykrishnamoorthy@gmail.com>
2026-01-27 21:46:06 +0000

    started my while loop

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -5,6 +5,11 @@ public class InsertionSort implements Sorter {
     public void sort(int[] input) {
         int n = input.length;
         for (int i = 1; i < n; i++){
+            int key = input[i];
+            int j = i - 1;
+
+            while (j >=0 && input[i] > key) {
+
         System.out.println("Insertion Sort!!!");
     }
 }
