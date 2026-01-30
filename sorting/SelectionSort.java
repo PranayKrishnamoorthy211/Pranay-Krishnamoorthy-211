6f47843fa65d4dc73e51dc2b29ee51f722fbac9e
PranayKrishnamoorthy211 <pranaykrishnamoorthy@gmail.com>
2026-01-30 21:39:28 +0000

    finished insertion part, finished with the project

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -1,8 +1,21 @@
 package sorting;
 
-public class SelectionSort implements Sorter {
+public class SelectionSort implements Sorter{
 
-    public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+    public void sort(int[] arr) {
+        int length = arr.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
