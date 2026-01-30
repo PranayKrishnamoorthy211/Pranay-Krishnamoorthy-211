a6c2077023de5eba1f8bed89949bac36e1ab53e9
PranayKrishnamoorthy211 <pranaykrishnamoorthy@gmail.com>
2026-01-30 22:05:47 +0000

    added a method to generate a random array. finished the project

--- sorting/Main.java
+++ sorting/Main.java
@@ -3,27 +3,20 @@ package sorting;
 public class Main {
     public static void main(String[] args)
     {
-        // Task:
-        // 1. Implement missing sorting algorithms.
-        //    - InsertionSort.java
-        //    - SelectionSort.java
-        //
-        //  2. Add a method to class Main to generate a random array.
-        //  3. Provide random input to test suite and ensure your implementations
-        //     of InsertionSort and Selection sort continue to work.
-        //  4. Increase the loop count for TestSuite to get better performance results.
 
         int[] testInput = randomArray(5);
 
-        // Increase the loop count to get better results once it works.
-        // TestSuite.run(testInput, 10000);
         TestSuite.run(testInput, 1);
     }
 
     public static int[] randomArray(int length)
     {
-        // TODO: make this return an actual random array of the provided length.
-        int[] a = {10, 5, 3, 2};
+        int[] a = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            int b = (int) Math.random() * 101;
+            a[i] = b;
+        }
         return a;
     }
 
