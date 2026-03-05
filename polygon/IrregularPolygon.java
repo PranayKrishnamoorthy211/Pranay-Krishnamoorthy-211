b91f38e3609a4827d838d524740ab834a0b862f1
PranayKrishnamoorthy211 <pranaykrishnamoorthy@gmail.com>
2026-03-05 22:42:37 +0000

    added code like for loops getting closer to completing the lab   :)

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -41,3 +41,26 @@ double dy = next.y - current.y;
 
 perimeter += Math.sqrt(dx * dx + dy * dy);
 }
+
+
+return perimeter;
+}
+
+// Calculate the area using the Shoelace Formula
+public double area() {
+double area = 0.0;
+
+if (myPolygon.size() < 3) {
+return 0.0;
+}
+
+for (int i = 0; i < myPolygon.size(); i++) {
+Point2D.Double current = myPolygon.get(i);
+Point2D.Double next;
+
+// Wrap around to first point
+if (i == myPolygon.size() - 1) {
+next = myPolygon.get(0);
+} else {
+next = myPolygon.get(i + 1);
+}
