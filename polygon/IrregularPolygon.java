83b67998a97a6d5ad9dd944a17a177f16490ff1a
PranayKrishnamoorthy211 <pranaykrishnamoorthy@gmail.com>
2026-03-05 22:27:05 +0000

    added code such as for loops variables, and doubles, almost done with the project

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -24,3 +24,20 @@ double perimeter = 0.0;
 if (myPolygon.size() < 2) {
 return 0.0;
 }
+
+for (int i = 0; i < myPolygon.size(); i++) {
+Point2D.Double current = myPolygon.get(i);
+Point2D.Double next;
+
+// If last point, connect back to first
+if (i == myPolygon.size() - 1) {
+next = myPolygon.get(0);
+} else {
+next = myPolygon.get(i + 1);
+}
+
+double dx = next.x - current.x;
+double dy = next.y - current.y;
+
+perimeter += Math.sqrt(dx * dx + dy * dy);
+}
