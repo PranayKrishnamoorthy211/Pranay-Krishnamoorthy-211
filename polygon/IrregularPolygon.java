bb75e4273091df12f6bb5bb6ec3bc5426a8762ad
PranayKrishnamoorthy211 <pranaykrishnamoorthy@gmail.com>
2026-03-06 02:06:23 +0000

    finished code, there was an error show i had to restart

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -1,7 +1,7 @@
 package polygon;
 
 import java.awt.geom.*; // for Point2D.Double
-import java.util.ArrayList; // for ArrayList
+import java.util.ArrayList;
 import gpdraw.*; // for DrawingTool
 
 public class IrregularPolygon {
@@ -12,8 +12,7 @@ private ArrayList<Point2D.Double> myPolygon = new ArrayList<Point2D.Double>();
 public IrregularPolygon() {}
 
 // Add a point to the polygon
-public void add(Point2D.Double aPoint)
-{
+public void add(Point2D.Double aPoint) {
 myPolygon.add(aPoint);
 }
 
@@ -26,22 +25,18 @@ return 0.0;
 }
 
 for (int i = 0; i < myPolygon.size(); i++) {
+
 Point2D.Double current = myPolygon.get(i);
 Point2D.Double next;
 
-// If last point, connect back to first
 if (i == myPolygon.size() - 1) {
 next = myPolygon.get(0);
 } else {
 next = myPolygon.get(i + 1);
 }
 
-double dx = next.x - current.x;
-double dy = next.y - current.y;
-
-perimeter += Math.sqrt(dx * dx + dy * dy);
+perimeter += current.distance(next);
 }
-
 
 return perimeter;
 }
@@ -55,17 +50,18 @@ return 0.0;
 }
 
 for (int i = 0; i < myPolygon.size(); i++) {
+
 Point2D.Double current = myPolygon.get(i);
 Point2D.Double next;
 
-// Wrap around to first point
 if (i == myPolygon.size() - 1) {
 next = myPolygon.get(0);
 } else {
 next = myPolygon.get(i + 1);
 }
 
-area += (current.x * next.y) - (current.y * next.x);
+area += (current.getX() * next.getY()) -
+(current.getY() * next.getX());
 }
 
 area = Math.abs(area) / 2.0;
@@ -74,29 +70,27 @@ return area;
 }
 
 // Draw the polygon
-public void draw()
-{
+public void draw() {
 try {
+
 if (myPolygon.size() < 2) {
 return;
 }
 
 DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
 
-// Move to first point
 Point2D.Double first = myPolygon.get(0);
+
 myDrawingTool.up();
-myDrawingTool.move(first.x, first.y);
+myDrawingTool.move(first.getX(), first.getY());
 myDrawingTool.down();
 
-// Draw lines to remaining points
 for (int i = 1; i < myPolygon.size(); i++) {
 Point2D.Double point = myPolygon.get(i);
-myDrawingTool.move(point.x, point.y);
+myDrawingTool.move(point.getX(), point.getY());
 }
 
-// Close polygon by returning to first point
-myDrawingTool.move(first.x, first.y);
+myDrawingTool.move(first.getX(), first.getY());
 
 } catch (java.awt.HeadlessException e) {
 System.out.println("Exception: No graphics support available.");
