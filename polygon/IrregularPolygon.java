2679d8e17e0be8264d68b8e37ea876d48f19fd45
PranayKrishnamoorthy211 <pranaykrishnamoorthy@gmail.com>
2026-03-07 02:43:37 +0000

    did the extra credit but the vnc wasn't working

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -1,78 +1,90 @@
+
 package polygon;
 
-import java.awt.Point;
-import java.awt.Polygon;
 import java.awt.geom.*;
 import java.util.ArrayList;
 import gpdraw.*;
 
 public class IrregularPolygon {
-private ArrayList<Point2D.Double> pointsList;
+    private ArrayList<ArrayList<Point2D.Double>> shapes;
+    private SketchPad pad;
+    private DrawingTool pen;
 
-public IrregularPolygon() {
-pointsList = new ArrayList<Point2D.Double>();
-}
+    public IrregularPolygon() {
+        shapes = new ArrayList<ArrayList<Point2D.Double>>();
+        shapes.add(new ArrayList<Point2D.Double>());
+        
+        // Initialize the drawing window once
+        pad = new SketchPad(500, 500);
+        pen = new DrawingTool(pad);
+    }
 
-public void add(Point2D.Double newPoint) {
-pointsList.add(newPoint);
-}
+    public void add(Point2D.Double newPoint) {
+        shapes.get(0).add(newPoint);
+    }
 
-public double perimeter() {
-if (pointsList.size() < 2) {
-return 0.0;
-}
+    public void addSmileyFace() {
+        // Right Eye
+        ArrayList<Point2D.Double> rEye = new ArrayList<Point2D.Double>();
+        rEye.add(new Point2D.Double(20, 40));
+        rEye.add(new Point2D.Double(30, 40));
+        rEye.add(new Point2D.Double(25, 50));
+        shapes.add(rEye);
 
-double totalPerimeter = 0.0;
+        // Left Eye
+        ArrayList<Point2D.Double> lEye = new ArrayList<Point2D.Double>();
+        lEye.add(new Point2D.Double(-20, 40));
+        lEye.add(new Point2D.Double(-30, 40));
+        lEye.add(new Point2D.Double(-25, 50));
+        shapes.add(lEye);
 
-for (int i = 0; i < pointsList.size(); i++) {
-Point2D.Double currentPoint = pointsList.get(i);
-Point2D.Double nextPoint = pointsList.get((i + 1) % pointsList.size());
-totalPerimeter += currentPoint.distance(nextPoint);
-}
+        // Mouth
+        ArrayList<Point2D.Double> mouth = new ArrayList<Point2D.Double>();
+        mouth.add(new Point2D.Double(-30, 10));
+        mouth.add(new Point2D.Double(0, -10));
+        mouth.add(new Point2D.Double(30, 10));
+        shapes.add(mouth);
+    }
 
-return totalPerimeter;
-}
+    public void draw() {
+        for (int i = 0; i < shapes.size(); i++) {
+            ArrayList<Point2D.Double> current = shapes.get(i);
+            
+            if (current.size() > 0) {
+                Point2D.Double first = current.get(0);
+                pen.up();
+                pen.move(first.getX(), first.getY());
+                pen.down();
 
-public double area() {
-if (pointsList.size() < 3) {
-return 0.0;
-}
+                for (int j = 1; j < current.size(); j++) {
+                    Point2D.Double pt = current.get(j);
+                    pen.move(pt.getX(), pt.getY());
+                }
 
-double calculationSum = 0.0;
+                // Close the polygon and the eyes
+                pen.move(first.getX(), first.getY());
+            }
+        }
+    }
 
-for (int i = 0; i < pointsList.size(); i++) {
-Point2D.Double pointA = pointsList.get(i);
-Point2D.Double pointB = pointsList.get((i + 1) % pointsList.size());
-calculationSum = calculationSum + (pointA.getX() * pointB.getY() - pointA.getY() * pointB.getX());
-}
+    public double perimeter() {
+        ArrayList<Point2D.Double> pts = shapes.get(0);
+        if (pts.size() < 2) return 0.0;
+        double total = 0.0;
+        for (int i = 0; i < pts.size(); i++) {
+            total += pts.get(i).distance(pts.get((i + 1) % pts.size()));
+        }
+        return total;
+    }
 
-return Math.abs(calculationSum / 2.0);
-}
-
-public void draw()
-{
-if (pointsList.size() < 2) {
-return;
-}
-
-try {
-DrawingTool pen = new DrawingTool(new SketchPad(500, 500));
-
-Point2D.Double startPoint = pointsList.get(0);
-
-pen.up();
-pen.move(startPoint.getX(), startPoint.getY());
-pen.down();
-
-for (int i = 1; i < pointsList.size(); i++) {
-Point2D.Double nextDrawPoint = pointsList.get(i);
-pen.move(nextDrawPoint.getX(), nextDrawPoint.getY());
-}
-
-pen.move(startPoint.getX(), startPoint.getY());
-
-} catch (java.awt.HeadlessException error) {
-System.out.println("Exception: No graphics support available.");
-}
-}
+    public double area() {
+        ArrayList<Point2D.Double> pts = shapes.get(0);
+        if (pts.size() < 3) return 0.0;
+        double sum = 0.0;
+        for (int i = 0; i < pts.size(); i++) {
+            sum += (pts.get(i).getX() * pts.get((i + 1) % pts.size()).getY() - 
+                    pts.get(i).getY() * pts.get((i + 1) % pts.size()).getX());
+        }
+        return Math.abs(sum / 2.0);
+    }
 }
